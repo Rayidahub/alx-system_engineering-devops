cbb4611ba070c1313785431d0d21e4f0aa2938ae
Rayidahub <rayidatech@gmail.com>
2023-05-22 15:32:14 -0400

    yes

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,31 +1,24 @@
 #!/usr/bin/python3
-'''A script that gathers employee name completed
-tasks and total number of tasks from an API
-'''
-
-import re
+"""returns information about a given employee ID.
+"""
 import requests
-import sys
+from sys import argv
 
-REST_API = "https://jsonplaceholder.typicode.com"
 
-if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        if re.fullmatch(r'\d+', sys.argv[1]):
-            id = int(sys.argv[1])
-            emp_req = requests.get('{}/users/{}'.format(REST_API, id)).json()
-            task_req = requests.get('{}/todos'.format(REST_API)).json()
-            emp_name = emp_req.get('name')
-            tasks = list(filter(lambda x: x.get('userId') == id, task_req))
-            completed_tasks = list(filter(lambda x: x.get('completed'), tasks))
-            print(
-                'Employee {} is done with tasks({}/{}):'.format(
-                    emp_name,
-                    len(completed_tasks),
-                    len(tasks)
-                )
-            )
-            if len(completed_tasks) > 0:
-                for task in completed_tasks:
-                    print('\t {}'.format(task.get('title')))
+if __name__ == "__main__":
+    """not execute when imports
+    """
+    url = "https://jsonplaceholder.typicode.com/"
+    request = requests.get("{}users/{}".format(url, argv[1])).json()
+    EMPLOYEE_NAME = request["name"]
+    TOTAL_NUMBER_OF_TASKS = len(requests.get("{}todos?userId={}"
+                                             .format(url, argv[1])).json())
+    NUMBER_OF_DONE_TASKS = len(requests.get("{}todos?userId={}&&completed=true"
+                                            .format(url, argv[1])).json())
+    print("Employee {} is done with tasks({}/{}):"
+          .format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+    DONE_TASKS = requests.get("{}todos?userId={}&&completed=true"
+                              .format(url, argv[1])).json()
+    for task in DONE_TASKS:
+        print("\t " + task["title"])
 

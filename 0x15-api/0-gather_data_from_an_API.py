2591957a3ae0905816aaeecbcd3e029945e6ea1d
Rayidahub <rayidatech@gmail.com>
2023-05-22 15:38:49 -0400

    test2

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,24 +1,74 @@
+Skip to content
+Product
+Solutions
+Open Source
+Pricing
+Search
+Sign in
+Sign up
+kwhit2
+/
+holberton-system_engineering-devops
+Public
+Code
+Issues
+Pull requests
+Actions
+Projects
+Security
+Insights
+holberton-system_engineering-devops/0x15-api/0-gather_data_from_an_API.py /
+@kwhit2
+kwhit2 pep8 fix
+Latest commit 796011b on May 3, 2021
+ History
+ 1 contributor
+Executable File  31 lines (29 sloc)  1.5 KB
+ 
+
 #!/usr/bin/python3
-"""returns information about a given employee ID.
-"""
+""" Python script that, using this REST API, for a given employee ID, returns
+    information about his/her TODO list progress. Requirements:
+    You must use urllib or requests module
+    The script must accept an integer as a parameter, which is the employee ID
+    The script must display on the standard output the employee TODO list...
+    ...progress in this exact format: First line: Employee EMPLOYEE_NAME is...
+    ...done with tasks(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):,
+    NUMBER_OF_DONE_TASKS: number of completed tasks, TOTAL_NUMBER_OF_TASKS:...
+    ...total number of tasks, which is the sum of completed and non-completed..
+    ..tasks Second and N next lines display the title of completed tasks:..
+    ..TASK_TITLE (with 1 tabulation and 1 space before the TASK_TITLE) """
 import requests
 from sys import argv
 
 
 if __name__ == "__main__":
-    """not execute when imports
-    """
-    url = "https://jsonplaceholder.typicode.com/"
-    request = requests.get("{}users/{}".format(url, argv[1])).json()
-    EMPLOYEE_NAME = request["name"]
-    TOTAL_NUMBER_OF_TASKS = len(requests.get("{}todos?userId={}"
-                                             .format(url, argv[1])).json())
-    NUMBER_OF_DONE_TASKS = len(requests.get("{}todos?userId={}&&completed=true"
-                                            .format(url, argv[1])).json())
-    print("Employee {} is done with tasks({}/{}):"
-          .format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
-    DONE_TASKS = requests.get("{}todos?userId={}&&completed=true"
-                              .format(url, argv[1])).json()
-    for task in DONE_TASKS:
-        print("\t " + task["title"])
-
+    userId = argv[1]
+    name = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                        .format(userId)).json()
+    # todo variable = grabs all todos (completed or not) for the user passed in
+    todo = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
+                        .format(userId)).json()
+    completed = []
+    for task in todo:
+        if task.get('completed') is True:
+            completed.append(task.get('title'))
+    print("Employee {} is done with tasks({}/{}):".
+          format(name.get('name'), len(completed), len(todo)))
+    for task in completed:
+        print("\t {}".format(task))
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
+holberton-system_engineering-devops/0-gather_data_from_an_API.py at master · kwhit2/holberton-system_engineering-devops · GitHub
